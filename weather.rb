c818d801a5aa733c39a2c736bb09f9b9f0ac8d3c
Antti Tapio <antti.tapio@northid.com>
2012-10-14 13:51:46 +0300

    tasker coordinate format support

--- weather.rb
+++ weather.rb
@@ -4,10 +4,11 @@ class Weather < Sinatra::Base
     location = Yr::Location.new(60.183, 24.933)
     location.current_weather.symbol.name
   end
-  get "/:lat/:lon" do
+  get "/:position" do
     content_type 'text/plain'
-    latitude = params[:lat].to_f
-    longitude = params[:lon].to_f
+    coordinates = params[:position].split(',')
+    latitude = coordinates[0].to_f
+    longitude = coordinates[1].to_f
     location = Yr::Location.new(latitude, longitude)
     location.current_weather.symbol.name
   end
