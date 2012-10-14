1531ce3fe0fbb3a274ff87ce253f12721ba91c3b
Antti Tapio <antti.tapio@northid.com>
2012-10-14 13:36:24 +0300

    support for dynamic coordinates

--- weather.rb
+++ weather.rb
@@ -4,4 +4,11 @@ class Weather < Sinatra::Base
     location = Yr::Location.new(60.183, 24.933)
     location.current_weather.symbol.name
   end
+  get "/:lat/:lon" do
+    content_type 'text/plain'
+    latitude = params[:lat].to_f
+    longitude = params[:lon].to_f
+    location = Yr::Location.new(latitude, longitude)
+    location.current_weather.symbol.name
+  end
 end
